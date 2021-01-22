ae6acb0234f5b4904fdd4b420da59acc13bf8850
Justin Haldeman <jmanh14@gmail.com>
2021-01-22 11:02:59 -0600

    added Fan model

---
+++ StarWarsApplication/Models/Fan.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWarsApplication.Models
+{
+    public class Fan
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BirthYear { get; set; }
+        public string EyeColor { get; set; }
+        public string Gender { get; set; }
+        public string HairColor { get; set; }
+        public string Species { get; set; }
+        public string HomeWorld { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
